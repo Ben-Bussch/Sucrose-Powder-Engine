286b356879fb0b5c789e53f3357209122db5af28
Ben <85500722+Ben-Bussch@users.noreply.github.com>
2023-10-21 23:53:40 +0100

    Added velocity calcs
    Have to incorporate heat transfer model

--- Blowout/AirBlowout.py
+++ Blowout/AirBlowout.py
@@ -32,7 +32,7 @@ class Solver:
         time = [t]
         mdot = [-Aexit*rho[0]*((self.gamma*self.R*T[0])**(1/2))*((2/(self.gamma+1))**((self.gamma+1)/(2*(self.gamma-1))))]
         print("mdot start: ", mdot[0])
-        
+        v = [mdot[0]/(rho[0]*Aexit)]
         
         count = 0
         
@@ -56,8 +56,9 @@ class Solver:
             
             #Temperature
             T1 = P1*T[count]/P[count]
-            T1 = self.T0
+            #T1 = self.T0
             T.append(T1)
+            
             
             #Density
             rho1 = m[count+1]/Vtank
@@ -66,11 +67,14 @@ class Solver:
             t += dt
             time.append(t)
             
+            #Velocity
+            v.append(mdot1/(rho[count]*Aexit))
+                        
             count += 1
 
         
         print("Time: ",t,"mass: ", m[count], "mdot: ", mdot[count], "Pressure: ",P[count])
-        return mdot, P,T, time
+        return mdot, P, T, v, time
     
     def fuelTank(self, dt, Aexit, VR, m0):
         """Numerically solves fuel tank blowout, using tank parameters and timestep dt"""
@@ -91,6 +95,7 @@ class Solver:
         time = [t]
         mdot = [-(rho_f*Aexit*np.sqrt(2*(Pstart-self.P0)/rho_f))]
         Vdot = [mdot[0]*rho_f]
+        v = [mdot[0]/(rho_f*Aexit)]
         print("mdot start: ", mdot[0])
         
         count = 0
@@ -123,10 +128,13 @@ class Solver:
             t += dt
             time.append(t)
             
+            #Velocity
+            v.append(mdot1/(rho_f*Aexit))
+            
             count += 1
             
         print("Time: ",t,"mass: ", m[count], "mdot: ", mdot[count], "Pressure: ",P[count])
-        return mdot, P, T, time
+        return mdot, P, T, v, time
         
     def OFratio(self, mdot_a,mdot_f):
         """Calculates OF ratio from air and fuel tank blowout"""
@@ -159,7 +167,7 @@ dt = 0.001
 
 SolverInstance = Solver(Pt)
 
-mdot_f, P_f, T_f, t = SolverInstance.fuelTank(dt, Ae_f, VR, m_f)
+mdot_f, P_f, T_f, v_f,  t = SolverInstance.fuelTank(dt, Ae_f, VR, m_f)
 
 """Air tank calcs"""
 V_a = 0.00806903       #m^3, volume of air tank
@@ -168,7 +176,7 @@ Cd_a = 0.8
 
 tf = t[-1]
 
-mdot_a, P_a, T_a, t_a = SolverInstance.airTank(dt, V_a, Ae_a, tf, Cd_a)
+mdot_a, P_a, T_a, v_a , t_a = SolverInstance.airTank(dt, V_a, Ae_a, tf, Cd_a)
 
 
 OF = SolverInstance.OFratio(mdot_a,mdot_f)
@@ -223,6 +231,16 @@ plt.xlabel("Time [s]")
 plt.ylabel("OF ratio")
 plt.savefig('OF_vs_time.png', dpi=300)
 
+plt.figure(5)
+plt.clf()
+plt.plot(t, v_f, label='Fuel Tank')
+plt.plot(t_a, v_a, label='Air Tank')
+
+plt.grid(1)
+plt.xlabel("Time [s]")
+plt.ylabel("Velocity [m s-1]")
+plt.savefig('Velocity_vs_time.png', dpi=300)
+plt.legend()
 
 plt.ion()
 
